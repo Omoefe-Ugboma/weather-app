13a4d17929553f5aed1bfec7445a24b1b35c778a
Omoefe Joseph Ugboma <jkomoefey23e@yahoo.co.uk>
2020-05-31 15:21:22 +0100

    adding styles and background image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+const api = {
+  key: "a678cc5d0d8a53b9cdf08242b83b4847",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 function App() {
   return (
-    <div className="App">
-     
+    <div className="app">
+       <main>
+         <div className="search-box">
+           <input
+            type="text"
+            className="search-bar"
+            placeholder="Seach..."
+           />
+         </div>
+       </main>
     </div>
   );
 }
 
 export default App;
+
+
