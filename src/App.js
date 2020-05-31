143854838d538d95e2c50491ed2cae66fd8f31f2
Omoefe Joseph Ugboma <jkomoefey23e@yahoo.co.uk>
2020-05-31 20:43:53 +0100

    Finally completed my Weather Report Application with React

--- src/App.js
+++ src/App.js
@@ -11,7 +11,11 @@ function App() {
     if(evt.key === "Enter"){
       fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
       .then(res => res.json())
-      .then(result => setWeather(result));
+      .then(result => {
+        setWeather(result);
+        setQuery('');
+        console.log(result);
+      });
     }
   }
 
@@ -28,25 +32,39 @@ function App() {
     return `${day} ${date} ${month} ${year}`
   }
   return (
-    <div className="app">
+    <div className={
+          (typeof weather.main != "undefined") 
+          ? ((weather.main.temp > 16) 
+          ? 'app warm' 
+          : 'app') 
+          : 'app'}>
        <main>
          <div className="search-box">
+           <p className="head">Weather Application For All Countries and Cities</p>
            <input
             type="text"
             className="search-bar"
             placeholder="Seach..."
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
            />
          </div>
-         <div className="location-box">
-         <div className="location">New York City, US</div>
-         <div className="date">{dateBuilder(new Date())}</div>
-         </div>
-         <div className="weather-box">
-          <div className="temp"> 
-            15°C
+         {(typeof weather.main != "undefined") ? (
+          <div>
+             <div className="location-box">
+              <div className="location">{weather.name}, {weather.sys.country}</div>
+              <div className="date">{dateBuilder(new Date())}</div>
+              </div>
+              <div className="weather-box">
+                <div className="temp"> 
+                  {/* 19°C */}
+                  {Math.round(weather.main.temp)}°C
+            </div>
+         <div className="weather">{weather.weather[0].main}</div>
           </div>
-          <div className="weather">Sunny</div>
-         </div>
+       </div>
+         ) : ('')}
        </main>
     </div>
   );
