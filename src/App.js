c23418a5b42881a943036859e75a7b6d859c08cc
Omoefe Joseph Ugboma <jkomoefey23e@yahoo.co.uk>
2020-05-31 19:30:50 +0100

    Create the search function and call the OpenWeather Api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState} from 'react';
 const api = {
   key: "a678cc5d0d8a53b9cdf08242b83b4847",
   base: "https://api.openweathermap.org/data/2.5/"
 }
 function App() {
+
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+  const search = evt => {
+    if(evt.key === "Enter"){
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+      .then(res => res.json())
+      .then(result => setWeather(result));
+    }
+  }
+
   const dateBuilder = (d) => {
     let months = ["January","February","March","April","May","June","July","August","September","October",
     "November","December"];
