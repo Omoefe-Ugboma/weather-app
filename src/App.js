7aa7f063e986973011757a237df5e10865cd87cc
Omoefe Joseph Ugboma <jkomoefey23e@yahoo.co.uk>
2020-05-31 19:06:16 +0100

    Finished With the usser-interface looks

--- src/App.js
+++ src/App.js
@@ -4,6 +4,18 @@ const api = {
   base: "https://api.openweathermap.org/data/2.5/"
 }
 function App() {
+  const dateBuilder = (d) => {
+    let months = ["January","February","March","April","May","June","July","August","September","October",
+    "November","December"];
+    let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
   return (
     <div className="app">
        <main>
@@ -13,6 +25,16 @@ function App() {
             className="search-bar"
             placeholder="Seach..."
            />
+         </div>
+         <div className="location-box">
+         <div className="location">New York City, US</div>
+         <div className="date">{dateBuilder(new Date())}</div>
+         </div>
+         <div className="weather-box">
+          <div className="temp"> 
+            15°C
+          </div>
+          <div className="weather">Sunny</div>
          </div>
        </main>
     </div>
