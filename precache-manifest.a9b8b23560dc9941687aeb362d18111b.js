fda2f16d267af7e944d074240986b3504b982513
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2021-01-02 09:57:13 +0100

    Updates

---
+++ precache-manifest.a9b8b23560dc9941687aeb362d18111b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4682f2a3d31cd2dcfc007ea608f841ab",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "a13e4ad027ccc0e28ea4",
+    "url": "/weather-app/static/css/main.4bb47973.chunk.css"
+  },
+  {
+    "revision": "1faad7f255ee893c55bc",
+    "url": "/weather-app/static/js/2.a4fb13ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.a4fb13ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a13e4ad027ccc0e28ea4",
+    "url": "/weather-app/static/js/main.65cf0098.chunk.js"
+  },
+  {
+    "revision": "490dbced286fae99269b",
+    "url": "/weather-app/static/js/runtime-main.74b9901d.js"
+  },
+  {
+    "revision": "0c1ff873cebf41463ca0bf2853ec643c",
+    "url": "/weather-app/static/media/WeathImage.0c1ff873.jpg"
+  },
+  {
+    "revision": "6f11e082f4f87fb774a4eb1eda30eb0e",
+    "url": "/weather-app/static/media/WeatherImageApp3.6f11e082.png"
+  }
+]);
